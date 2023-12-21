5619bd061c623e8072dcef82ac1d004b4ce928c1
Slawomir Slowik <slawomirslowik@yahoo.pl>
2023-12-21 08:35:49 +0100

    init

---
+++ pages/search_page.py
@@ -0,0 +1,23 @@
+from selenium.webdriver.support import expected_conditions as EC
+from pages.base_page import BasePage
+from data.locators import SearchPageLocators
+
+
+class SearchPage(BasePage):
+
+    def __init__(self, driver, wait):
+        self.url = "https://duckduckgo.com/"
+        self.locator = SearchPageLocators
+        super().__init__(driver, wait)
+
+    def go_to_search_page(self):
+        self.go_to_page(self.url)
+
+    def check_title(self, title):
+        self.wait.until(EC.title_contains(title))
+
+    def make_a_search(self, input_text):
+        self.driver.find_element(*self.locator.SEARCH_INPUT).send_keys(input_text)
+        self.driver.find_element(*self.locator.SEARCH_BUTTON).click()
+        self.wait.until(EC.presence_of_element_located(self.locator.RESULTS))
+        self.driver.save_screenshot("results/results.png")
